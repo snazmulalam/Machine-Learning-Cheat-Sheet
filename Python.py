7314094c962bad8dc567e8356afcf043bd1a9d5d
Shah Md Nazmul Alam <shahmdnazmulalam@gmail.com>
2023-09-23 21:15:17 +0600

    Initial Commit

--- Python.py
+++ Python.py
@@ -6,8 +6,7 @@ for i in range(1, 365):
         d = str(i) + "days ago"
         with open('file.txt', 'a') as file:
             file.write(d)
-
-        os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "commit"')
+            os.system('git add .')
+            os.system('git commit --date="' + d + '" -m "commit"')
 
     os.system('git push -u origin main')
